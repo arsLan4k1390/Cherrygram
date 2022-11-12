1a40bc52cd884dd9ed5145e5ab72a7c8741e58b4
arsLan4k1390 <arslan4k1390@gmail.com>
2022-11-12 22:59:44 +0500

    Update ConnectionsManager.java

--- TMessagesProj/src/main/java/org/telegram/tgnet/ConnectionsManager.java
+++ TMessagesProj/src/main/java/org/telegram/tgnet/ConnectionsManager.java
@@ -346,7 +346,7 @@ public class ConnectionsManager extends BaseController {
                         if (BuildVars.LOGS_ENABLED) {
                             FileLog.d("java received " + resp + " error = " + error);
                         }
-                        FileLog.dumpResponseAndRequest(object, resp, error, requestMsgId, finalStartRequestTime, requestToken);
+                        //FileLog.dumpResponseAndRequest(object, resp, error, requestMsgId, finalStartRequestTime, requestToken);
                         final TLObject finalResponse = resp;
                         final TLRPC.TL_error finalError = error;
                         Utilities.stageQueue.postRunnable(() -> {
@@ -560,7 +560,7 @@ public class ConnectionsManager extends BaseController {
             buff.reused = true;
             int constructor = buff.readInt32(true);
             final TLObject message = TLClassStore.Instance().TLdeserialize(buff, constructor, true);
-            FileLog.dumpUnparsedMessage(message, messageId);
+            //FileLog.dumpUnparsedMessage(message, messageId);
             if (message instanceof TLRPC.Updates) {
                 if (BuildVars.LOGS_ENABLED) {
                     FileLog.d("java received " + message);
